227610de69ebd3a667c603db33dbddbe0c7b613e
Marianne Lyssand <marianne.lyssand@gmail.com>
2021-04-01 21:12:42 +0200

    Testing

---
+++ components/top-friends.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import TopFriendsStyle from "./top-friends.module.css";
+
+export default function Nav() {
+  return (
+    <div className={TopFriendsStyle.container}>
+        <p>Hei</p>
+    </div>
+  );
+}
