2f130360897d2f35eea82ef72b7e7b5ae3fe6d49
Marianne Lyssand <marianne.lyssand@gmail.com>
2021-04-02 22:12:05 +0200

    Link på "profile"

--- components/top-friends.js
+++ components/top-friends.js
@@ -7,81 +7,80 @@ export default function Nav() {
       <h2 className="content-header">Top friends</h2>
 
       <div className={TopFriendsStyle.friendContainer}>
-      <div className={TopFriendsStyle.friend}>
-        <Link href="/my-profile">
-          <a>
-            <img
-              className={TopFriendsStyle.friendProfilePic}
-              alt="friend profile pic"
-              src="/images/Joachim90.jpg"
-            />
-          </a>
-        </Link>
-        <div>
-          <div className={TopFriendsStyle.friendName}>
-            <p>Thomas Lund</p>
-          </div>
-          <div className={TopFriendsStyle.friendOptions}>
-            <p className="thin-text">Invite</p>
-            <p className="thin-text">Profile</p>
-          </div>
-        </div>
-      </div>
+        <div className={TopFriendsStyle.friend}>
+          <img
+            className={TopFriendsStyle.friendProfilePic}
+            alt="friend profile pic"
+            src="/images/Joachim90.jpg"
+          />
 
-      <div className={TopFriendsStyle.friend}>
-        <Link href="/my-profile">
-          <a>
-            <img
-              className={TopFriendsStyle.friendProfilePic}
-              alt="friend profile pic"
-              src="/images/Joachim90.jpg"
-            />
-          </a>
-        </Link>
-        <div>
-          <div className={TopFriendsStyle.friendName}>
-            <p>Sebastian Ali</p>
-          </div>
-          <div className={TopFriendsStyle.friendOptions}>
-            <p className="thin-text">Invite</p>
-            <p className="thin-text">Profile</p>
+          <div>
+            <div className={TopFriendsStyle.friendName}>
+              <p>Thomas Lund</p>
+            </div>
+            <div className={TopFriendsStyle.friendOptions}>
+              <p className="thin-text">Invite</p>
+              <Link href="/my-profile">
+                <a>
+                  <p className="thin-text">Profile</p>
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={TopFriendsStyle.friend}>
-        <Link href="/my-profile">
-          <a>
-            <img
-              className={TopFriendsStyle.friendProfilePic}
-              alt="friend profile pic"
-              src="/images/Joachim90.jpg"
-            />
-          </a>
-        </Link>
-        <div>
-          <div className={TopFriendsStyle.friendName}>
-            <p>Therese Engh</p>
-          </div>
-          <div className={TopFriendsStyle.friendOptions}>
-            <p className="thin-text">Invite</p>
-            <p className="thin-text">Profile</p>
+        <div className={TopFriendsStyle.friend}>
+          <img
+            className={TopFriendsStyle.friendProfilePic}
+            alt="friend profile pic"
+            src="/images/Joachim90.jpg"
+          />
+
+          <div>
+            <div className={TopFriendsStyle.friendName}>
+              <p>Sebastian Ali</p>
+            </div>
+            <div className={TopFriendsStyle.friendOptions}>
+              <p className="thin-text">Invite</p>
+              <Link href="/my-profile">
+                <a>
+                  <p className="thin-text">Profile</p>
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={TopFriendsStyle.buttonContainer}>
-        <div className={TopFriendsStyle.allFriends}>
+
+        <div className={TopFriendsStyle.friend}>
+          <img
+            className={TopFriendsStyle.friendProfilePic}
+            alt="friend profile pic"
+            src="/images/Joachim90.jpg"
+          />
+
+          <div>
+            <div className={TopFriendsStyle.friendName}>
+              <p>Therese Engh</p>
+            </div>
+            <div className={TopFriendsStyle.friendOptions}>
+              <p className="thin-text">Invite</p>
+              <Link href="/my-profile">
+                <a>
+                  <p className="thin-text">Profile</p>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className={TopFriendsStyle.buttonContainer}>
+          <div className={TopFriendsStyle.allFriends}>
             <p>All friends</p>
-        </div>
-        <div className={TopFriendsStyle.addFriends}>
+          </div>
+          <div className={TopFriendsStyle.addFriends}>
             <p>Add friends</p>
+          </div>
         </div>
       </div>
-
-      </div>
-
-
-
     </div>
   );
 }
