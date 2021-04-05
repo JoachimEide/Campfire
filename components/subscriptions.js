8d6ddd5730e195f1b0c037f099ee88387f19381d
Marianne Lyssand <marianne.lyssand@gmail.com>
2021-04-05 21:12:51 +0200

    Litt lengre på manage subscriptions

--- components/subscriptions.js
+++ components/subscriptions.js
@@ -7,24 +7,65 @@ export default function Subscriptions() {
 
         <div className={SubscriptionsStyle.subContainer}>
 
-          <div classsName={SubscriptionsStyle.subRow}>
-            <img className={SubscriptionsStyle.subLogo}
-            alt="netflix logo"
-            src="/images/DisneyPlusLogo.svg"
-            />
+          <div className={SubscriptionsStyle.subRow}>
+
+            <div className={SubscriptionsStyle.subProvider}>
+              <img className={SubscriptionsStyle.subLogo}
+              alt="netflix logo"
+              src="/images/DisneyPlusLogo.svg"
+              />
+              
+              <div className={SubscriptionsStyle.subNamePrice}>
+                <p>Netflix</p>
+                <p>109,- per month</p>
+              </div>
+
+              <div className={SubscriptionsStyle.subActiveEdit}>
+                <p className={SubscriptionsStyle.subActiveState}>Active</p>
+                <p>Edit</p>
+              </div>
+            </div>
+
+            <div className={SubscriptionsStyle.subProvider}>
+              <img className={SubscriptionsStyle.subLogo}
+              alt="netflix logo"
+              src="/images/DisneyPlusLogo.svg"
+              />
+              
+              <div className={SubscriptionsStyle.subNamePrice}>
+                <p>NRK TV</p>
+                <p>Free</p>
+              </div>
+
+              <div className={SubscriptionsStyle.subActiveEditNRK}>
+                <p className={SubscriptionsStyle.subActiveState}>Active</p>
+                <p>Edit</p>
+              </div>
+            </div>
+
+            <div className={SubscriptionsStyle.subProvider}>
+              <img className={SubscriptionsStyle.subLogo}
+              alt="NRK logo"
+              src="/images/DisneyPlusLogo.svg"
+              />
+              
+              <div className={SubscriptionsStyle.subNamePrice}>
+                <p>Viaplay</p>
+                <p>109,- per month</p>
+              </div>
+
+              <div className={SubscriptionsStyle.subActiveEdit}>
+                <p className={SubscriptionsStyle.subActiveState}>Active</p>
+                <p>Edit</p>
+              </div>
+            </div>
             
-            <div>
-              <p>Netflix</p>
-              <p>109,- per month</p>
-            </div>
-            <div>
-              <p>Active</p>
-              <p>Edit</p>
-            </div>
+
           </div>
 
+          <div className={SubscriptionsStyle.subRow}>
 
-          <div classsName={SubscriptionsStyle.subRow}></div>
+          </div>
           <div classsName={SubscriptionsStyle.subAddTotalRow}></div>
         </div>
 
