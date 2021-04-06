0dba17cad7c80ab771a58cd6a0bd60bb7e05bcd1
JoachimEide <joachim.woll.eide@gmail.com>
2021-04-06 14:16:15 +0200

    add play button

--- components/subscriptions.js
+++ components/subscriptions.js
@@ -1,76 +1,70 @@
-import SubscriptionsStyle from "./subscriptions.module.css"
+import SubscriptionsStyle from "./subscriptions.module.css";
 
 export default function Subscriptions() {
-    return (
-      <div className={SubscriptionsStyle.container}>
-        <h2 className="content-header">Manage subscriptions</h2>
+  return (
+    <div className={SubscriptionsStyle.container}>
+      <h2 className="content-header">Manage subscriptions</h2>
 
-        <div className={SubscriptionsStyle.subContainer}>
-
-          <div className={SubscriptionsStyle.subRow}>
-
-            <div className={SubscriptionsStyle.subProvider}>
-              <img className={SubscriptionsStyle.subLogo}
+      <div className={SubscriptionsStyle.subContainer}>
+        <div className={SubscriptionsStyle.subRow}>
+          <div className={SubscriptionsStyle.subProvider}>
+            <img
+              className={SubscriptionsStyle.subLogo}
               alt="netflix logo"
               src="/images/DisneyPlusLogo.svg"
-              />
-              
-              <div className={SubscriptionsStyle.subNamePrice}>
-                <p>Netflix</p>
-                <p>109,- per month</p>
-              </div>
+            />
 
-              <div className={SubscriptionsStyle.subActiveEdit}>
-                <p className={SubscriptionsStyle.subActiveState}>Active</p>
-                <p>Edit</p>
-              </div>
+            <div className={SubscriptionsStyle.subNamePrice}>
+              <p>Netflix</p>
+              <p>109,- per month</p>
             </div>
 
-            <div className={SubscriptionsStyle.subProvider}>
-              <img className={SubscriptionsStyle.subLogo}
+            <div className={SubscriptionsStyle.subActiveEdit}>
+              <p className={SubscriptionsStyle.subActiveState}>Active</p>
+              <p>Edit</p>
+            </div>
+          </div>
+
+          <div className={SubscriptionsStyle.subProvider}>
+            <img
+              className={SubscriptionsStyle.subLogo}
               alt="netflix logo"
               src="/images/DisneyPlusLogo.svg"
-              />
-              
-              <div className={SubscriptionsStyle.subNamePrice}>
-                <p>NRK TV</p>
-                <p>Free</p>
-              </div>
+            />
 
-              <div className={SubscriptionsStyle.subActiveEditNRK}>
-                <p className={SubscriptionsStyle.subActiveState}>Active</p>
-                <p>Edit</p>
-              </div>
+            <div className={SubscriptionsStyle.subNamePrice}>
+              <p>NRK TV</p>
+              <p>Free</p>
             </div>
 
-            <div className={SubscriptionsStyle.subProvider}>
-              <img className={SubscriptionsStyle.subLogo}
+            <div className={SubscriptionsStyle.subActiveEditNRK}>
+              <p className={SubscriptionsStyle.subActiveState}>Active</p>
+              <p>Edit</p>
+            </div>
+          </div>
+
+          <div className={SubscriptionsStyle.subProvider}>
+            <img
+              className={SubscriptionsStyle.subLogo}
               alt="NRK logo"
               src="/images/DisneyPlusLogo.svg"
-              />
-              
-              <div className={SubscriptionsStyle.subNamePrice}>
-                <p>Viaplay</p>
-                <p>109,- per month</p>
-              </div>
+            />
 
-              <div className={SubscriptionsStyle.subActiveEdit}>
-                <p className={SubscriptionsStyle.subActiveState}>Active</p>
-                <p>Edit</p>
-              </div>
+            <div className={SubscriptionsStyle.subNamePrice}>
+              <p>Viaplay</p>
+              <p>109,- per month</p>
             </div>
-            
 
+            <div className={SubscriptionsStyle.subActiveEdit}>
+              <p className={SubscriptionsStyle.subActiveState}>Active</p>
+              <p>Edit</p>
+            </div>
           </div>
-
-          <div className={SubscriptionsStyle.subRow}>
-
-          </div>
-          <div classsName={SubscriptionsStyle.subAddTotalRow}></div>
         </div>
 
+        <div className={SubscriptionsStyle.subRow}></div>
+        <div className={SubscriptionsStyle.subAddTotalRow}></div>
       </div>
-
-
-    );
-  }
+    </div>
+  );
+}
--- components/titlepage/titlepage.js
+++ components/titlepage/titlepage.js
@@ -1,12 +1,25 @@
 import TitleNav from "./titleNav";
 import Sosialbar from "./titleSosial";
+import TitlePageStyle from "./titlepage.module.css";
 
 export default function Layout(props) {
   return (
     <div className="wrapper">
       <TitleNav serviceSrc="/images/DisneyPlusLogo.svg" />
       <Sosialbar />
-      <div className="content"></div>
+      <div className="content">
+        <div className={TitlePageStyle.titleTop}>
+          <img
+            className={TitlePageStyle.showLogo}
+            src="/images/mandalorian_logo.svg"
+            alt="mando logo"
+          />
+          <button className={TitlePageStyle.playButton}>Play</button>
+          <button className={TitlePageStyle.watchlistButton}>
+            Add to watchlist
+          </button>
+        </div>
+      </div>
       <style jsx>{`
         .content {
           background-image: linear-gradient(
