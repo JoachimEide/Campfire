72be6b0ab324854e5c3b120a37adcd97b892abf5
Marianne Lyssand <marianne.lyssand@gmail.com>
2021-04-08 22:24:51 +0200

    profile-details

--- components/profile-details.js
+++ components/profile-details.js
@@ -5,10 +5,34 @@ export default function ProfileDetails() {
     <div className={ProfileDetailsStyle.container}>
       <h2 className="content-header">Pia Henriksen</h2>
 
-      <img 
-      className={ProfileDetailsStyle.profilePic}
-      alt="profile pic" 
-      src="/images/Joachim90.jpg" />
+      <img
+        className={ProfileDetailsStyle.profilePic}
+        alt="profile pic"
+        src="/images/Joachim90.jpg"
+      />
+
+      <div className={ProfileDetailsStyle.text}>
+        <div className={ProfileDetailsStyle.textAndIcon}>
+          <p>pia.henriksen@gmail.com</p>
+          <img
+            className={ProfileDetailsStyle.pencilIcon}
+            alt="profile pic"
+            src="/images/pencil.svg"
+          />
+        </div>
+        <div className={ProfileDetailsStyle.textAndIcon}>
+          <p>Password: ********</p>
+          <img
+            className={ProfileDetailsStyle.pencilIcon}
+            alt="profile pic"
+            src="/images/pencil.svg"
+          />
+        </div>
+        <p>Payment</p>
+        <p>App settings</p>
+        <p>Help</p>
+        <p>Sign out</p>
+      </div>
     </div>
   );
 }
