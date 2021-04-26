bf93f680d35c3b463ed6acf303120bce5512ee23
Marianne Lyssand <marianne.lyssand@gmail.com>
2021-04-26 16:36:50 +0200

    Watched this

--- data/friends.js
+++ data/friends.js
@@ -6,16 +6,44 @@ export const friendsData = [
     online: true,
     imgSrc: "/images/friends/thomas-lund.jpeg",
     nowWatching: {
-      id: 1,
+      id: 13,
       title: "Mandalorian",
       seasonAndEpisode: "S2E6",
       service: "Disney+",
     },
-    friends: [3, 5, 2],
+    friends: [4, 3, 5, 2],
     watchList: [19, 23, 22, 82],
     watchHistory: [1, 46, 50, 53, 12],
-    watchedShow: [{ id: 1, seasonAndEpisode: "-S2E6", service: "Disney+" }],
-    reviews: [{ id: 1, score: 9, reviewText: "I really enjoy this show!" }, { id: 13, score: 10, reviewText: "One of the best shows I've seen in a long time! #babyyoda" }, { id: 21, score: 7, reviewText: "Overall good, but a little dissapointed in the final season" } ],
+    watchedShow: [
+      { id: 13, seasonAndEpisode: "S2E5", service: "Disney+" },
+      { id: 10, seasonAndEpisode: "", service: "Disney+" },
+      { id: 2, seasonAndEpisode: "", service: "HBO Nordic" },
+      { id: 21, seasonAndEpisode: "S6E18", service: "HBO Nordic" },
+      { id: 23, seasonAndEpisode: "S1E8", service: "Disney+" },
+      { id: 16, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 8, seasonAndEpisode: "S2E8", service: "HBO Nordic" },
+      { id: 89, seasonAndEpisode: "S3E5", service: "NRK" },
+      { id: 1, seasonAndEpisode: "S1E6", service: "Disney+" },
+      { id: 14, seasonAndEpisode: "S1E1", service: "HBO Nordic" },
+      { id: 12, seasonAndEpisode: "S2E6", service: "Netflix" },
+    ],
+    reviews: [
+      { id: 1, 
+        score: 9, 
+        reviewText: "I really enjoy this show!" 
+      },
+      {
+        id: 13,
+        score: 10,
+        reviewText: "One of the best shows I've seen in a long time! #babyyoda",
+      },
+      {
+        id: 21,
+        score: 7,
+        reviewText:
+          "Overall good, but a little dissapointed in the final season",
+      },
+    ],
   },
   {
     id: 2,
@@ -29,11 +57,40 @@ export const friendsData = [
       seasonAndEpisode: "S1E4",
       service: "Disney+",
     },
-    friends: [3, 1, 5],
+    friends: [3, 1, 5, 4],
     watchList: [46, 30, 51, 8],
     watchHistory: [5, 47, 45, 55, 21],
-    watchedShow: [{ id: 1, seasonAndEpisode: "-S2E6", service: "Disney+" }],
-    reviews: [{ id: 5, score: 8, reviewText: "Quite nervewrecking this one!" }, { id: 19, score: 8, reviewText: "Started a little slow, but got better and better" }, { id: 10, score: 6, reviewText: "I'm a huge Pixar fan, so I expected a little more" } ],
+    watchedShow: [
+      { id: 13, seasonAndEpisode: "S2E6", service: "Disney+" },
+      { id: 1, seasonAndEpisode: "S1E6", service: "Disney+" },
+      { id: 21, seasonAndEpisode: "S6E18", service: "HBO Nordic" },
+      { id: 23, seasonAndEpisode: "S1E8", service: "Disney+" },
+      { id: 16, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 8, seasonAndEpisode: "S2E8", service: "HBO Nordic" },
+      { id: 89, seasonAndEpisode: "S3E5", service: "NRK" },
+      { id: 59, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 42, seasonAndEpisode: "", service: "Netflix" },
+      { id: 73, seasonAndEpisode: "S1E1", service: "Tv2 Sumo" },
+      { id: 14, seasonAndEpisode: "S1E1", service: "HBO Nordic" },
+      { id: 6, seasonAndEpisode: "", service: "Tv2 Sumo" },
+      
+    ],
+    reviews: [
+      { id: 5, 
+        score: 8, 
+        reviewText: "Quite nervewrecking this one!" 
+      },
+      {
+        id: 19,
+        score: 8,
+        reviewText: "Started a little slow, but got better and better",
+      },
+      {
+        id: 10,
+        score: 6,
+        reviewText: "I'm a huge Pixar fan, so I expected a little more",
+      },
+    ],
   },
   {
     id: 3,
@@ -47,11 +104,39 @@ export const friendsData = [
       seasonAndEpisode: "S5E5",
       service: "Netflix",
     },
-    friends: [1, 4, 3],
+    friends: [1, 4, 5, 2],
     watchList: [16, 81, 22, 92],
     watchHistory: [15, 96, 10, 8, 13],
-    watchedShow: [{ id: 1, seasonAndEpisode: "-S2E6", service: "Disney+" }],
-    reviews: [{ id: 37, score: 10, reviewText: "I love all the Toy Story movies, this did not dissapoint either :)" }, { id: 45, score: 8, reviewText: "Why couldn't they have nailed the ending on this awesome show?" }, { id: 15, score: 7, reviewText: "I really enjoy this show!" } ],
+    watchedShow: [
+      { id: 13, seasonAndEpisode: "S2E6", service: "Disney+" },
+      { id: 17, seasonAndEpisode: "", service: "Netflix" },
+      { id: 9, seasonAndEpisode: "S1E1", service: "Netflix" },
+      { id: 55, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 48, seasonAndEpisode: "S2E1", service: "HBO Nordic" },
+      { id: 25, seasonAndEpisode: "", service: "Netflix" },
+      { id: 70, seasonAndEpisode: "", service: "Tv2 Sumo" },
+      { id: 6, seasonAndEpisode: "", service: "Tv2 Sumo" },
+      { id: 12, seasonAndEpisode: "S2E6", service: "Netflix" },
+
+    ],
+    reviews: [
+      {
+        id: 37,
+        score: 10,
+        reviewText:
+          "I love all the Toy Story movies, this did not dissapoint either :)",
+      },
+      {
+        id: 45,
+        score: 8,
+        reviewText:
+          "Why couldn't they have nailed the ending on this awesome show?",
+      },
+      { id: 15, 
+        score: 7, 
+        reviewText: "I really enjoy this show!" 
+      },
+    ],
   },
   {
     id: 4,
@@ -65,11 +150,38 @@ export const friendsData = [
       seasonAndEpisode: "",
       service: "Campfire",
     },
-    friends: [5, 3, 2],
+    friends: [5, 1, 3, 2],
     watchList: [9, 1, 14, 48, 3],
     watchHistory: [24, 15, 89, 48],
-    watchedShow: [{ id: 1, seasonAndEpisode: "-S2E6", service: "Disney+" }],
-    reviews: [{ id: 38, score: 8, reviewText: "If you like Grey's anatomy, I really recommend this show." }, { id: 24, score: 10, reviewText: "Best series I've seen in forever! Love the chemistry between the characters!" }, { id: 42, score: 9, reviewText: "So funny! Surprised that I liked it that much. JaJa Ding Dong! " } ],
+    watchedShow: [
+      { id: 13, seasonAndEpisode: "S2E6", service: "Disney+" },
+      { id: 17, seasonAndEpisode: "", service: "Netflix" },
+      { id: 9, seasonAndEpisode: "S1E1", service: "Netflix" },
+      { id: 55, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 48, seasonAndEpisode: "S2E1", service: "HBO Nordic" },
+      { id: 25, seasonAndEpisode: "", service: "Netflix" },
+      { id: 70, seasonAndEpisode: "", service: "Tv2 Sumo" },
+      { id: 1, seasonAndEpisode: "S1E6", service: "Disney+" },
+      { id: 3, seasonAndEpisode: "S1E4", service: "NRK" },
+    ],
+    reviews: [
+      {
+        id: 38,
+        score: 8,
+        reviewText: "If you like Grey's anatomy, I really recommend this show.",
+      },
+      {
+        id: 24,
+        score: 10,
+        reviewText:
+          "Best series I've seen in forever! Love the chemistry between the characters!",
+      },
+      {
+        id: 89,
+        score: 10,
+        reviewText: "This show is something else. I couldn't stop watching!",
+      },
+    ],
   },
   {
     id: 5,
@@ -78,10 +190,37 @@ export const friendsData = [
     online: false,
     imgSrc: "/images/friends/therese-eng.jpeg",
     nowWatching: { id: false, title: "Offline", episode: "", service: "" },
-    friends: [2, 1, 3],
+    friends: [4, 2, 1, 3],
     watchList: [2, 48, 13, 12],
     watchHistory: [82, 32, 24, 25],
-    watchedShow: [{ id: 1, seasonAndEpisode: "-S2E6", service: "Disney+" }],
-    reviews: [{ id: 23, score: 9, reviewText: "A must if you like Marvel movies!" }, { id: 89, score: 10, reviewText: "This show is something else. I couldn't stop watching!" }, { id: 82, score: 7, reviewText: "Dark and twisted, worth a watch." } ],
+    watchedShow: [
+      { id: 13, seasonAndEpisode: "S2E6", service: "Disney+" },
+      { id: 10, seasonAndEpisode: "", service: "Disney+" },
+      { id: 17, seasonAndEpisode: "", service: "Netflix" },
+      { id: 9, seasonAndEpisode: "S1E1", service: "Netflix" },
+      { id: 59, seasonAndEpisode: "", service: "Viaplay" },
+      { id: 42, seasonAndEpisode: "", service: "Netflix" },
+      { id: 73, seasonAndEpisode: "S1E1", service: "Tv2 Sumo" },
+      { id: 1, seasonAndEpisode: "S1E6", service: "Disney+" },
+      { id: 89, seasonAndEpisode: "S3E5", service: "NRK" },
+      { id: 3, seasonAndEpisode: "S1E4", service: "NRK" },
+      { id: 6, seasonAndEpisode: "", service: "Tv2 Sumo" },
+    ],
+    reviews: [
+      { id: 23, 
+        score: 9, 
+        reviewText: "A must if you like Marvel movies!" 
+      },
+      {
+        id: 42,
+        score: 9,
+        reviewText:
+          "So funny! Surprised that I liked it that much. JaJa Ding Dong! ",
+      },
+      { id: 82, 
+        score: 7, 
+        reviewText: "Dark and twisted, worth a watch." 
+      },
+    ],
   },
 ];
